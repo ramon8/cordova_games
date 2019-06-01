a1d3e67587f0a0c11d2172ca4987c96799569d1b
ramon8 <ramon.fer96@gmail.com>
2019-06-01 19:45:42 +0200

    arregla esto sergi

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,30 +1,60 @@
+
+const ratio_w = screen.width / 10
+const ratio_h = screen.height / (screen.height / ratio_w);
+
+const aspect_ratio = screen.width / screen.height;
+
+const canvas_height = ratio_w * Math.trunc(screen.height / ratio_w);
+
 document.addEventListener('deviceready', function () {
-    game = new Phaser.Game(50, 50, Phaser.CANVAS, 'game');
-    // var game = new Phaser.Game(
-    //     50,
-    //     50,
-    //     Phaser.CANVAS,
-    //     'game',
-    //     {
-    //         preload: preload,
-    //         create: create,
-    //         update: update,
-    //         render: render
-    //     });
-
-    // function preload() {
-
-    // }
-
-    // function create() {
-
-    // }
-
-    // function update() {
-
-    // }
-
-    // function render() {
-
-    // }
+    game = new Phaser.Game(screen.width, canvas_height, Phaser.CANVAS, 'game', {
+        preload: preload, create: create, update: update, render: render
+    });
 });
+
+function preload() {
+    game.load.spritesheet('pj_iddle', 'img/assets/pj_iddle.png', 35, 70);
+    game.load.spritesheet('floor', 'img/assets/suelo.png', 16, 16);
+}
+
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.stage.backgroundColor = '#000000';
+
+    pj = game.add.sprite(50, 50, 'pj_iddle', 2);
+    pj.smoothed = false;
+    pj.scale.setTo(2);
+    floor = [];
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < Math.trunc(screen.height / ratio_w); j++) {
+            if (j == 0 || i == 0 || j == Math.trunc(screen.height / ratio_w) -1 || i == 9) {
+                var floorTemp = game.add.sprite(i * ratio_w, j * ratio_w, 'floor', 0);
+                floorTemp.scale.setTo(ratio_w/16);
+                floorTemp.smoothed = false;
+                floor.push(floor);
+            }
+        }
+    }
+
+    game.physics.arcade.enable([pj]);
+
+    game.physics.arcade.gravity.y = 200;
+
+    pj.body.bounce.y = 0.95;
+    pj.body.collideWorldBounds = true;
+    pj.body.allowGravity = true;
+
+    var walk = pj.animations.add('walk');
+
+    pj.animations.play('walk', 15, true);
+
+
+}
+
+function update() {
+    //game.physics.arcade.collide(pj, floor);
+}
+
+function render() {
+}
--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,25 +1,48 @@
+
 document.addEventListener('deviceready', function () {
-    game = new Phaser.Game(300, 300, Phaser.CANVAS, 'game', {
+    game = new Phaser.Game(screen.width, screen.height, Phaser.CANVAS, 'game', {
         preload: preload, create: create, update: update, render: render
     });
 });
 
 function preload() {
-    console.log('preload');
+    game.load.spritesheet('pj_iddle', 'img/assets/pj_iddle.png', 35, 70);
+    // game.load.spritesheet('floor', 'img/assets/suelo.jpg', 16, 16);
 }
 
 function create() {
-
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.stage.backgroundColor = '#123123';
-    game.stage.backgroundColor = '#ffffff';
-    console.log('create');
+
+	game.stage.backgroundColor = '#000000cc';
+
+	pj = game.add.sprite(0, 0, 'pj_iddle', 2);
+	// floor = game.add.sprite(0, 0, 'floor', 1);
+    // floor = [];
+    for(var i = 0; i < 10; i++){
+        for(var j = 0; j < 15; j++){
+            //floor.push(game.add.sprite(i * 16, j * 16, 'floor', 0));
+        }
+    }
+    game.add.sprite(0, 0, 'pj_iddle', 0);
+
+	game.physics.arcade.enable([pj]);
+
+	game.physics.arcade.gravity.y = 200;
+
+	pj.body.bounce.y = 0.95;
+	pj.body.collideWorldBounds = true;
+    pj.body.allowGravity = true;
+    
+    var walk = pj.animations.add('walk');
+
+    pj.animations.play('walk', 15, true);
+    
+
 }
 
 function update() {
-    console.log('update');
+	//game.physics.arcade.collide(pj, floor);
 }
 
 function render() {
-    console.log('render');
 }
