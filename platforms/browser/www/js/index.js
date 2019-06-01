95a739183d7fcf8e5c1cce3ed92256ca4d1404e3
ramon8 <ramon.fer96@gmail.com>
2019-06-01 18:06:31 +0200

    ciclo de vida funciones

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,4 +1,25 @@
 document.addEventListener('deviceready', function () {
-    game = new Phaser.Game(50, 50, Phaser.CANVAS, 'game');
-    
+    game = new Phaser.Game(300, 300, Phaser.CANVAS, 'game', {
+        preload: preload, create: create, update: update, render: render
+    });
 });
+
+function preload() {
+    console.log('preload');
+}
+
+function create() {
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.stage.backgroundColor = '#123123';
+    game.stage.backgroundColor = '#ffffff';
+    console.log('create');
+}
+
+function update() {
+    console.log('update');
+}
+
+function render() {
+    console.log('render');
+}
